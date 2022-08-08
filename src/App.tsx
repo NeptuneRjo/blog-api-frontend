774906c1f79aac292ec2f17e18a959cf582daaef
NeptuneRjo <rjo9000@outlook.com>
2022-08-08 05:24:02 -0400

    import bootswatch theme and impliment Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Navbar } from './components/components-exports'
+import 'bootswatch/dist/morph/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
     <div className="app-main">
+      <Navbar />
     </div>
   );
 }
