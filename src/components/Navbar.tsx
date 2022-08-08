92d344b8e64437f133de0ddc1d4877bf37c1ba5f
NeptuneRjo <rjo9000@outlook.com>
2022-08-08 19:19:26 -0400

    add nav link to create a new blog

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,21 +2,22 @@ import React from 'react'
 import { Container, Nav, Navbar as NavBar } from 'react-bootstrap'
 
 const Navbar = () => {
-  return (
-    <NavBar bg='light' expand='lg' sticky='top'>
-        <Container>
-            <NavBar.Brand href='/blogs'>Blog API</NavBar.Brand>
-            <NavBar.Toggle aria-controls='basic-navbar-nav' />
-            <NavBar.Collapse id='basic-navbar-nav'>
-                <Nav className='me-auto'>
-                    <Nav.Link href='/log-in'>Log In</Nav.Link>
-                    <Nav.Link href='/sign-out'>Sign Out</Nav.Link>
-                    <Nav.Link href='/sign-up'>Sign Up</Nav.Link>
-                </Nav>
-            </NavBar.Collapse>
-        </Container>  
-    </NavBar>
-  )
+	return (
+		<NavBar bg='light' expand='lg' sticky='top'>
+			<Container>
+				<NavBar.Brand href='/blogs'>Blog API</NavBar.Brand>
+				<NavBar.Toggle aria-controls='basic-navbar-nav' />
+				<NavBar.Collapse id='basic-navbar-nav'>
+					<Nav className='me-auto'>
+						<Nav.Link href='/log-in'>Log In</Nav.Link>
+						<Nav.Link href='/sign-out'>Sign Out</Nav.Link>
+						<Nav.Link href='/sign-up'>Sign Up</Nav.Link>
+						<Nav.Link href='/create'>Create a blog</Nav.Link>
+					</Nav>
+				</NavBar.Collapse>
+			</Container>
+		</NavBar>
+	)
 }
 
 export default Navbar
