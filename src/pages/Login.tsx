f86b0cb1cd17e71e985a40ee14ea6bd224906192
NeptuneRjo <rjo9000@outlook.com>
2022-08-08 19:08:17 -0400

    add basic form structure for log in page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const Login = () => {
+	return (
+		<Form>
+			<Form.Group className='mb-3' controlId='formBasicEmail'>
+				<Form.Label>Email address</Form.Label>
+				<Form.Control type='email' placeholder='Enter email' />
+			</Form.Group>
+			<Form.Group className='mb-3' controlId='formBasicPassword'>
+				<Form.Label>Password</Form.Label>
+				<Form.Control type='password' placeholder='Enter password' />
+			</Form.Group>
+			<Button type='submit'>Log in</Button>
+		</Form>
+	)
+}
+
+export default Login
