3ea10a4603925d9d13598b06fc0548abe3d92748
NeptuneRjo <rjo9000@outlook.com>
2022-08-08 22:04:41 -0400

    add dashboard page to exports

--- src/pages/pages-exports.tsx
+++ src/pages/pages-exports.tsx
@@ -1,3 +1,4 @@
 export { default as Signup } from './Signup'
 export { default as Login } from './Login'
 export { default as CreateBlog } from './CreateBlog'
+export { default as Dashboard } from './Dashboard'
