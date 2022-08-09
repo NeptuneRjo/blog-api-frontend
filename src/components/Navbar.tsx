0d357794b4d941a0fd25b7ae5886bd5eb5fbb77e
NeptuneRjo <rjo9000@outlook.com>
2022-08-09 00:10:02 -0400

    add correct hrefs to Nav Links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Container, Nav, Navbar as NavBar } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 	return (
 		<NavBar bg='light' expand='lg' sticky='top'>
 			<Container>
-				<NavBar.Brand href='/blogs'>Blog API</NavBar.Brand>
+				<NavBar.Brand href='/api'>Blog API</NavBar.Brand>
 				<NavBar.Toggle aria-controls='basic-navbar-nav' />
 				<NavBar.Collapse id='basic-navbar-nav'>
 					<Nav className='me-auto'>
-						<Nav.Link href='/log-in'>Log In</Nav.Link>
-						<Nav.Link href='/sign-out'>Sign Out</Nav.Link>
-						<Nav.Link href='/sign-up'>Sign Up</Nav.Link>
-						<Nav.Link href='/create'>Create a blog</Nav.Link>
+						<Nav.Link href='#/log-in'>Log In</Nav.Link>
+						<Nav.Link>Sign Out</Nav.Link>
+						<Nav.Link href='#/sign-up'>Sign Up</Nav.Link>
+						<Nav.Link href='#/create'>Create a blog</Nav.Link>
 					</Nav>
 				</NavBar.Collapse>
 			</Container>
