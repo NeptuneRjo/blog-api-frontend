2e66c4a8f4997f71ea26c83481eaa2d028ffe518
NeptuneRjo <rjo9000@outlook.com>
2022-08-10 03:25:04 -0400

    add props and display data from backend

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Blog } from '../App'
 
-const Dashboard = () => {
-	return <div>Dashboard</div>
+export type Props = {
+	blogs: Blog[] // the prop blogs has the type 'Array of blogs'
+}
+
+const Dashboard: React.FC<Props> = ({ blogs }: Props) => {
+	return (
+		<div>
+			{blogs.map((blog) => (
+				<div>{blog.title}</div>
+			))}
+		</div>
+	)
 }
 
 export default Dashboard
