4468173b5739d0fe7dfcb46581aef1d2d98a5458
NeptuneRjo <rjo9000@outlook.com>
2022-08-10 17:20:12 -0400

    add blogs as props to blogcard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Blog } from '../App'
+import { Blog } from '../App' // Interface
+import { BlogCard } from '../components/components-exports'
 
 export type Props = {
 	blogs: Blog[] // the prop blogs has the type 'Array of blogs'
@@ -7,10 +8,10 @@ export type Props = {
 
 const Dashboard: React.FC<Props> = ({ blogs }: Props) => {
 	return (
-		<div>
-			{blogs.map((blog) => (
-				<div>{blog.title}</div>
-			))}
+		<div className='dashboard-main'>
+			<h3 className='page-title'>Blogs</h3>
+			{blogs.length > 0 && blogs.map((blog) => <BlogCard blog={blog} />)}
+			{blogs.length === 0 && <p>No blogs to display.</p>}
 		</div>
 	)
 }
