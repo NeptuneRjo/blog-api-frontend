b6637c73b12e72cf933f914925869e2f13e17eb0
NeptuneRjo <rjo9000@outlook.com>
2022-08-10 03:24:45 -0400

    add fetch for blogs on app render, created typings for blogs to be used when props are passed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Navbar } from './components/components-exports'
 import { CreateBlog, Signup, Login, Dashboard } from './pages/pages-exports'
 import { Route, Routes } from 'react-router-dom'
@@ -7,7 +7,34 @@ import { HashRouter } from 'react-router-dom'
 import 'bootswatch/dist/morph/bootstrap.min.css'
 import './App.css'
 
-function App() {
+// The Blog objects that are returned from the backend
+export interface Blog {
+	_v: number
+	_id: string
+	author: string
+	body: string
+	comments: []
+	date: string
+	title: string
+}
+
+const App: React.FC = () => {
+	const [blogs, setBlogs] = useState<Blog[] | []>([])
+	// Must include the empty array type because this starts out as an empty array, and will be if no blogs are retrieved.
+	// State MUST start as an array because map is used in the *Dashboard* child
+	// Blog[] represents an array of the Blog interface (blog object)
+
+	useEffect(() => {
+		const fetchBlogs = async (): Promise<void> => {
+			const response: Response = await fetch('/api/blogs')
+			const json = await response.json()
+
+			setBlogs(json)
+		}
+
+		fetchBlogs()
+	}, [])
+
 	return (
 		<HashRouter>
 			<div className='app-main'>
@@ -16,7 +43,7 @@ function App() {
 					<Route path='create' element={<CreateBlog />} />
 					<Route path='sign-up' element={<Signup />} />
 					<Route path='log-in' element={<Login />} />
-					<Route path='/' element={<Dashboard />} />
+					<Route path='/' element={<Dashboard blogs={blogs} />} />
 				</Routes>
 			</div>
 		</HashRouter>
