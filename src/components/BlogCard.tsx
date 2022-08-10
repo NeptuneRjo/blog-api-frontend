fe904461ac7ee648611a49ac0757fc0522ffb621
NeptuneRjo <rjo9000@outlook.com>
2022-08-10 19:53:03 -0400

    add bootstrap card components

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { Blog } from '../App'
+import { Card, Button } from 'react-bootstrap'
+
+import formatDistanceToNow from 'date-fns/esm/formatDistanceToNow/index.js'
 
 export type Props = {
 	blog: Blog // blog prop has the typing of the Blog interface (blog object)
 }
 
 const BlogCard: React.FC<Props> = ({ blog }: Props) => {
-	return <div>{blog.title}</div>
+	return (
+		<Card>
+			<Card.Body>
+				<Card.Header as='h5'>{blog.title.toUpperCase()}</Card.Header>
+				<Card.Text style={{ margin: '1rem 0rem' }}>
+					<div>Written by {blog.author}</div>
+					<div>
+						{formatDistanceToNow(new Date(blog.date), { addSuffix: true })}
+					</div>
+				</Card.Text>
+				<Button variant='primary'>Visit blog</Button>
+			</Card.Body>
+		</Card>
+	)
 }
 
 export default BlogCard
