2393736111d0c76e34d1302bb9d1e8913eb72669
NeptuneRjo <rjo9000@outlook.com>
2022-08-10 22:59:30 -0400

    add HTML to style; fixed form inputs

--- src/pages/CreateBlog.tsx
+++ src/pages/CreateBlog.tsx
@@ -3,21 +3,24 @@ import { Form, Button } from 'react-bootstrap'
 
 const CreateBlog = () => {
 	return (
-		<Form>
-			<Form.Group className='mb-3' controlId='formBasicEmail'>
-				<Form.Label>Blog Title</Form.Label>
-				<Form.Control type='email' placeholder='Enter a title' />
-			</Form.Group>
-			<Form.Group className='mb-3' controlId='formBasicPassword'>
-				<Form.Label>Body</Form.Label>
-				<Form.Control
-					type='textarea'
-					aria-label='Blog Body'
-					placeholder='Write your blog'
-				/>
-			</Form.Group>
-			<Button type='submit'>Create blog</Button>
-		</Form>
+		<div className='create main'>
+			<h3 className='title'>Create blog</h3>
+			<Form>
+				<Form.Group className='mb-3' controlId='formBasicEmail'>
+					<Form.Label>Blog Title</Form.Label>
+					<Form.Control type='text' placeholder='Enter a title' />
+				</Form.Group>
+				<Form.Group className='mb-3' controlId='ControlTextarea1'>
+					<Form.Label>Body</Form.Label>
+					<Form.Control
+						as='textarea'
+						aria-label='Blog Body'
+						placeholder='Write your blog'
+					/>
+				</Form.Group>
+				<Button type='submit'>Create blog</Button>
+			</Form>
+		</div>
 	)
 }
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,17 +3,20 @@ import { Form, Button } from 'react-bootstrap'
 
 const Login = () => {
 	return (
-		<Form>
-			<Form.Group className='mb-3' controlId='formBasicEmail'>
-				<Form.Label>Email address</Form.Label>
-				<Form.Control type='email' placeholder='Enter email' />
-			</Form.Group>
-			<Form.Group className='mb-3' controlId='formBasicPassword'>
-				<Form.Label>Password</Form.Label>
-				<Form.Control type='password' placeholder='Enter password' />
-			</Form.Group>
-			<Button type='submit'>Log in</Button>
-		</Form>
+		<div className='login main'>
+			<h3 className='title'>Log in</h3>
+			<Form>
+				<Form.Group className='mb-3' controlId='formBasicEmail'>
+					<Form.Label>Email address</Form.Label>
+					<Form.Control type='email' placeholder='Enter email' />
+				</Form.Group>
+				<Form.Group className='mb-3' controlId='formBasicPassword'>
+					<Form.Label>Password</Form.Label>
+					<Form.Control type='password' placeholder='Enter password' />
+				</Form.Group>
+				<Button type='submit'>Log in</Button>
+			</Form>
+		</div>
 	)
 }
 
