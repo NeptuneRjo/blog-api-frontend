a9231957c9de9a0db4263378079c556837cd6d92
NeptuneRjo <rjo9000@outlook.com>
2022-08-12 16:26:20 -0400

    add comment and comment form to exports

--- src/components/components-exports.tsx
+++ src/components/components-exports.tsx
@@ -1,2 +1,4 @@
 export { default as Navbar } from './Navbar'
 export { default as BlogCard } from './BlogCard'
+export { default as Comment } from './Comment'
+export { default as CommentForm } from './CommentForm'
