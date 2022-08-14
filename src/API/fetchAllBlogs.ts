b02a05d9da286b2a17e9c6dedfb767600b01f7c8
NeptuneRjo <rjo9000@outlook.com>
2022-08-13 21:40:56 -0400

    test

--- src/API/fetchAllBlogs.ts
+++ src/API/fetchAllBlogs.ts
@@ -1,9 +1,7 @@
 import { BlogInt } from '../types'
 
 const fetchAllBlogs = async (): Promise<BlogInt[]> => {
-	const response: Response = await fetch(
-		'https://neptunerjo-blog-api.herokuapp.com/api/blogs'
-	)
+	const response: Response = await fetch('/api/blogs')
 	const json: BlogInt[] = await response.json()
 
 	return json
