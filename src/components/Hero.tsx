9aa93d08d995c78091dcf37a5b1f1a7e9fb59e29
NeptuneRjo <rjo9000@outlook.com>
2022-08-14 23:13:01 -0400

    add hero card

---
+++ src/components/Hero.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+	return (
+		<div className='hero-main'>
+			<div className='col-lg-6'>
+				<h1 className='display-5 fw-bold lh-1 mb-3'>
+					Lorem ipsum dolor sit amet.
+				</h1>
+				<p className='lead'>
+					Id porta nibh venenatis cras sed felis eget velit. Dui nunc mattis
+					enim ut tellus. Egestas congue quisque egestas diam in arcu cursus.
+					Feugiat in fermentum posuere urna nec tincidunt praesent semper.
+					Egestas purus viverra accumsan in nisl nisi scelerisque eu.
+				</p>
+			</div>
+		</div>
+	)
+}
+
+export default Hero
