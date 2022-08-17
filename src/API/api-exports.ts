64e050c4a8c0c333ad96f9f0b818c36e53cb3ade
NeptuneRjo <rjo9000@outlook.com>
2022-08-17 17:54:10 -0400

    add fetchUser to exports file

--- src/API/api-exports.ts
+++ src/API/api-exports.ts
@@ -1,3 +1,4 @@
 export { default as fetchBlog } from './fetchBlog'
 export { default as updateBlogWithNewComment } from './updateBlog'
 export { default as fetchAllBlogs } from './fetchAllBlogs'
+export { default as fetchUser } from './fetchUser'
