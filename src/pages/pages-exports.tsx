3f9083944d9b67b5f33bb79ae599a0834623350f
NeptuneRjo <rjo9000@outlook.com>
2022-08-17 23:40:13 -0400

    add signup and login pages to exports file

--- src/pages/pages-exports.tsx
+++ src/pages/pages-exports.tsx
@@ -1,2 +1,4 @@
 export { default as Dashboard } from './Dashboard'
 export { default as Blog } from './Blog'
+export { default as Login } from './Login'
+export { default as Signup } from './Signup'
