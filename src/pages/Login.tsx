a58fc0f7bd0ff8df1df3540e3ba3531efc8b7ad6
NeptuneRjo <rjo9000@outlook.com>
2022-08-18 03:50:14 -0400

    add handler for login submit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { CleanUserInt } from '../types'
+import { loginUser } from '../API/api-exports'
 
-const Login = () => {
+type Props = {
+	setUser: React.Dispatch<React.SetStateAction<CleanUserInt | undefined>>
+}
+
+const Login: React.FC<Props> = ({ setUser }: Props) => {
+	const [email, setEmail] = useState<string>('')
+	const [password, setPassword] = useState<string>('')
+	const [error, setError] = useState<any>()
+
+	const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		try {
+			const fetchedUser = await loginUser(email, password)
+			setUser(fetchedUser)
+
+			setEmail('')
+			setPassword('')
+		} catch (err) {
+			setError(err)
+		}
+	}
+
 	return (
-		<Form>
+		<Form onSubmit={(e) => handleFormSubmit(e)}>
 			<Form.Group className='mb-3' controlId='formBasicEmail'>
 				<Form.Label>Email address</Form.Label>
-				<Form.Control type='email' placeholder='Enter email' />
+				<Form.Control
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					type='email'
+					placeholder='Enter email'
+				/>
 			</Form.Group>
 			<Form.Group className='mb-3' controlId='formBasicPassword'>
 				<Form.Label>Password</Form.Label>
-				<Form.Control type='password' placeholder='Enter password' />
+				<Form.Control
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					type='password'
+					placeholder='Enter password'
+				/>
 			</Form.Group>
 			<Button type='submit'>Submit</Button>
 		</Form>
