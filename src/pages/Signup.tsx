363988cb8800e5be37cd2f199c29d85f85ae2907
NeptuneRjo <rjo9000@outlook.com>
2022-08-18 19:49:54 -0400

    add username field, passed to signupUser

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,24 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { signupUser } from '../API/api-exports'
+import { CleanUserInt } from '../types'
 
-const Signup = () => {
+type Props = {
+	setUser: React.Dispatch<React.SetStateAction<CleanUserInt | null>>
+}
+
+const Signup: React.FC<Props> = ({ setUser }: Props) => {
+	const [email, setEmail] = useState<string>('')
+	const [emailCheck, setEmailCheck] = useState<string>('')
+
+	const [password, setPassword] = useState<string>('')
+	const [passwordCheck, setPasswordCheck] = useState<string>('')
+
+	const [username, setUsername] = useState<string>('')
+
+	const [error, setError] = useState<unknown | null>(null)
+
+	const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+
+		if (email !== emailCheck || password !== passwordCheck) {
+			return
+		}
+
+		try {
+			const signedUpUser = await signupUser(email, password, username)
+			setUser(signedUpUser)
+
+			setEmail('')
+			setEmailCheck('')
+			setPassword('')
+			setPasswordCheck('')
+			setUsername('')
+		} catch (err) {
+			setError(err)
+		}
+	}
+
 	return (
-		<Form>
+		<Form onSubmit={(e) => handleFormSubmit(e)}>
 			<Form.Group className='mb-3' controlId='formBasicEmail'>
 				<Form.Label>Email address</Form.Label>
-				<Form.Control type='email' placeholder='Enter email' />
-			</Form.Group>
-			<Form.Group className='mb-3' controlId='formBasicEmail'>
+				<Form.Control
+					onChange={(e) => setEmail(e.target.value)}
+					type='email'
+					placeholder='Enter email'
+					value={email}
+				/>
 				<Form.Label>Confirm email address</Form.Label>
-				<Form.Control type='email' placeholder='Confirm email' />
+				<Form.Control
+					onChange={(e) => setEmailCheck(e.target.value)}
+					type='email'
+					placeholder='Confirm email'
+					value={emailCheck}
+				/>
 			</Form.Group>
 			<Form.Group className='mb-3' controlId='formBasicPassword'>
 				<Form.Label>Password</Form.Label>
-				<Form.Control type='password' placeholder='Enter password' />
-			</Form.Group>
-			<Form.Group className='mb-3' controlId='formBasicPassword'>
+				<Form.Control
+					onChange={(e) => setPassword(e.target.value)}
+					type='password'
+					placeholder='Enter password'
+					value={password}
+				/>
 				<Form.Label>Confirm password</Form.Label>
-				<Form.Control type='password' placeholder='Confirm password' />
+				<Form.Control
+					onChange={(e) => setPasswordCheck(e.target.value)}
+					type='password'
+					placeholder='Confirm password'
+					value={passwordCheck}
+				/>
+			</Form.Group>
+			<Form.Group className='mb-3' controlId='formBasicText'>
+				<Form.Label>Enter username</Form.Label>
+				<Form.Control
+					onChange={(e) => setUsername(e.target.value)}
+					type='text'
+					placeholder='Enter username'
+					value={username}
+				/>
 			</Form.Group>
 			<Button type='submit'>Submit</Button>
 		</Form>
