5fdb2e4a2e86622e3f8431b6f4873d4621c558d9
NeptuneRjo <rjo9000@outlook.com>
2022-08-19 01:25:30 -0400

    fix typing; impliment blog deletion

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { fetchBlog, updateBlogWithNewComment } from '../API/api-exports'
+import {
+	fetchBlog,
+	updateBlogWithNewComment,
+	deleteBlog,
+} from '../API/api-exports'
 import { CommentInt, BlogInt, CleanUserInt } from '../types'
 import { Comment, CommentForm } from '../components/components-exports'
+import { Button } from 'react-bootstrap'
+import history from 'history/browser'
 
 import formatDistanceToNow from 'date-fns/esm/formatDistanceToNow/index.js'
 
 type Props = {
-	user: CleanUserInt | null
+	user: CleanUserInt | undefined
+	setBlogs: React.Dispatch<React.SetStateAction<[] | BlogInt[]>>
 }
 
-const Blog: React.FC<Props> = ({ user }: Props) => {
+const Blog: React.FC<Props> = ({ user, setBlogs }: Props) => {
 	const { id } = useParams()
 
 	const [blog, setBlog] = useState<BlogInt | null>(null)
 	const [comments, setComments] = useState<CommentInt[] | []>([])
 	const [error, setError] = useState<unknown | null>(null)
+
+	const [deleting, setDeleting] = useState<boolean>(false)
 
 	useEffect(() => {
 		;(async function setStateToReturnedBlog() {
@@ -55,6 +64,17 @@ const Blog: React.FC<Props> = ({ user }: Props) => {
 		}
 	}
 
+	const handleBlogDelete = async () => {
+		try {
+			const newBlogList = await deleteBlog(id as string)
+			setBlogs(newBlogList)
+			setDeleting(false)
+			history.back()
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
 	return (
 		<div className='blog main'>
 			{blog !== null && (
@@ -67,6 +87,19 @@ const Blog: React.FC<Props> = ({ user }: Props) => {
 						</h6>
 						<p className='blog text'>{blog?.body}</p>
 					</div>
+					{user !== undefined && user.role === 'Admin' && (
+						<div className='blog delete-field'>
+							{deleting === false && (
+								<Button onClick={() => setDeleting(true)}>Delete Blog</Button>
+							)}
+							{deleting === true && (
+								<>
+									<Button onClick={() => handleBlogDelete()}>Delete</Button>
+									<Button onClick={() => setDeleting(false)}>Cancel</Button>
+								</>
+							)}
+						</div>
+					)}
 					<div className='blog comments'>
 						<h3 className='title'>Comments</h3>
 						<div className='blog comments-new'>
