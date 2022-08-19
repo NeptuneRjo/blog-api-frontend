9c0b821cd7e5df26e272c4ff627042009280075b
NeptuneRjo <rjo9000@outlook.com>
2022-08-19 17:14:43 -0400

    add content for signed up users

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
-import { signupUser } from '../API/api-exports'
+import { signupUser, logoutUser } from '../API/api-exports'
 import { CleanUserInt } from '../types'
 
 type Props = {
 	setUser: React.Dispatch<React.SetStateAction<CleanUserInt | undefined>>
+	user: CleanUserInt | undefined
 }
 
-const Signup: React.FC<Props> = ({ setUser }: Props) => {
+const Signup: React.FC<Props> = ({ setUser, user }: Props) => {
 	const [email, setEmail] = useState<string>('')
 	const [emailCheck, setEmailCheck] = useState<string>('')
 
@@ -39,51 +40,68 @@ const Signup: React.FC<Props> = ({ setUser }: Props) => {
 		}
 	}
 
+	const handleLogout = async (): Promise<void> => {
+		const loggedOut = await logoutUser()
+
+		setUser(loggedOut)
+	}
+
 	return (
-		<Form onSubmit={(e) => handleFormSubmit(e)}>
-			<Form.Group className='mb-3' controlId='formBasicEmail'>
-				<Form.Label>Email address</Form.Label>
-				<Form.Control
-					onChange={(e) => setEmail(e.target.value)}
-					type='email'
-					placeholder='Enter email'
-					value={email}
-				/>
-				<Form.Label>Confirm email address</Form.Label>
-				<Form.Control
-					onChange={(e) => setEmailCheck(e.target.value)}
-					type='email'
-					placeholder='Confirm email'
-					value={emailCheck}
-				/>
-			</Form.Group>
-			<Form.Group className='mb-3' controlId='formBasicPassword'>
-				<Form.Label>Password</Form.Label>
-				<Form.Control
-					onChange={(e) => setPassword(e.target.value)}
-					type='password'
-					placeholder='Enter password'
-					value={password}
-				/>
-				<Form.Label>Confirm password</Form.Label>
-				<Form.Control
-					onChange={(e) => setPasswordCheck(e.target.value)}
-					type='password'
-					placeholder='Confirm password'
-					value={passwordCheck}
-				/>
-			</Form.Group>
-			<Form.Group className='mb-3' controlId='formBasicText'>
-				<Form.Label>Enter username</Form.Label>
-				<Form.Control
-					onChange={(e) => setUsername(e.target.value)}
-					type='text'
-					placeholder='Enter username'
-					value={username}
-				/>
-			</Form.Group>
-			<Button type='submit'>Submit</Button>
-		</Form>
+		<div className='auth-page'>
+			{user === undefined && (
+				<Form onSubmit={(e) => handleFormSubmit(e)}>
+					<Form.Group className='mb-3' controlId='formBasicEmail'>
+						<Form.Label>Email address</Form.Label>
+						<Form.Control
+							onChange={(e) => setEmail(e.target.value)}
+							type='email'
+							placeholder='Enter email'
+							value={email}
+						/>
+						<Form.Label>Confirm email address</Form.Label>
+						<Form.Control
+							onChange={(e) => setEmailCheck(e.target.value)}
+							type='email'
+							placeholder='Confirm email'
+							value={emailCheck}
+						/>
+					</Form.Group>
+					<Form.Group className='mb-3' controlId='formBasicPassword'>
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							onChange={(e) => setPassword(e.target.value)}
+							type='password'
+							placeholder='Enter password'
+							value={password}
+						/>
+						<Form.Label>Confirm password</Form.Label>
+						<Form.Control
+							onChange={(e) => setPasswordCheck(e.target.value)}
+							type='password'
+							placeholder='Confirm password'
+							value={passwordCheck}
+						/>
+					</Form.Group>
+					<Form.Group className='mb-3' controlId='formBasicText'>
+						<Form.Label>Enter username</Form.Label>
+						<Form.Control
+							onChange={(e) => setUsername(e.target.value)}
+							type='text'
+							placeholder='Enter username'
+							value={username}
+						/>
+					</Form.Group>
+					<Button type='submit'>Submit</Button>
+				</Form>
+			)}
+			{user !== undefined && (
+				<div className='action-text'>
+					<p>You are currently signed in as {user.username}</p>
+					<Button href='#/'>View blogs</Button>
+					<Button onClick={() => handleLogout()}>Log out</Button>
+				</div>
+			)}
+		</div>
 	)
 }
 
