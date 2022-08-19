0aa4ba793308343c837178433001d5d78bda34eb
NeptuneRjo <rjo9000@outlook.com>
2022-08-18 23:03:45 -0400

    add user props to automatically include the accounts username if logged in

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,18 +1,24 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { CleanUserInt } from '../types'
 
 type Props = {
 	handleCommentSubmit: (username: string, body: string) => void
+	user: CleanUserInt | null
 }
 
-const CommentForm: React.FC<Props> = ({ handleCommentSubmit }: Props) => {
+const CommentForm: React.FC<Props> = ({ handleCommentSubmit, user }: Props) => {
 	const [username, setUsername] = useState<string>('')
 	const [body, setBody] = useState<string>('')
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 
-		handleCommentSubmit(username, body)
+		if (user !== null) {
+			handleCommentSubmit(user.username, body)
+		} else {
+			handleCommentSubmit(username, body)
+		}
 
 		setBody('')
 		setUsername('')
@@ -29,14 +35,16 @@ const CommentForm: React.FC<Props> = ({ handleCommentSubmit }: Props) => {
 					value={body}
 				/>
 			</Form.Group>
-			<Form.Group className='mb-3' controlId='formBasicAuthor'>
-				<Form.Label>Username</Form.Label>
-				<Form.Control
-					placeholder='Enter your username'
-					onChange={(e) => setUsername(e.target.value)}
-					value={username}
-				/>
-			</Form.Group>
+			{user === null && (
+				<Form.Group className='mb-3' controlId='formBasicAuthor'>
+					<Form.Label>Username</Form.Label>
+					<Form.Control
+						placeholder='Enter your username'
+						onChange={(e) => setUsername(e.target.value)}
+						value={username}
+					/>
+				</Form.Group>
+			)}
 			<Button type='submit'>Submit</Button>
 		</Form>
 	)
