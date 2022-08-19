2fe7154a937e5f8c2d0f6662f770c99169a9f38e
NeptuneRjo <rjo9000@outlook.com>
2022-08-19 01:23:45 -0400

    add handler for deleting blogs

---
+++ src/API/deleteBlog.ts
@@ -0,0 +1,19 @@
+import { BlogInt } from '../types'
+
+const deleteBlog = async (id: string) => {
+	const url = 'https://whispering-tundra-62913.herokuapp.com'
+
+	const response: Response = await fetch(`/api/blogs/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+		credentials: 'same-origin',
+	})
+
+	const json: BlogInt[] = await response.json()
+
+	return json
+}
+
+export default deleteBlog
