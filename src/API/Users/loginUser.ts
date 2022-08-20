c6a22e4fca1b3a0b86799f30b2d3ed00c069bdf4
NeptuneRjo <rjo9000@outlook.com>
2022-08-20 16:28:44 -0400

    move user-related API functions to its own folder

---
+++ src/API/Users/loginUser.ts
@@ -0,0 +1,26 @@
+import { CleanUserInt } from '../../types'
+
+const loginUser = async (
+	email: string,
+	password: string
+): Promise<CleanUserInt | undefined> => {
+	const url = 'https://whispering-tundra-62913.herokuapp.com'
+
+	const response: Response = await fetch(`/api/users/login`, {
+		method: 'POST',
+		body: JSON.stringify({
+			email,
+			password,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+		credentials: 'same-origin',
+	})
+
+	const json: CleanUserInt = await response.json()
+
+	return json
+}
+
+export default loginUser
