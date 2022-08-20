2cf930d930578dfca3524c8fda924726567b85c8
NeptuneRjo <rjo9000@outlook.com>
2022-08-20 16:27:43 -0400

    move blog-related API functions to its own folder

---
+++ src/API/Blogs/createBlog.ts
@@ -0,0 +1,28 @@
+import { BlogInt } from '../../types'
+
+const createBlog = async (
+	title: string,
+	body: string,
+	author: string
+): Promise<BlogInt[] | []> => {
+	const url = 'https://whispering-tundra-62913.herokuapp.com'
+
+	const response: Response = await fetch(`/api/blogs`, {
+		method: 'POST',
+		body: JSON.stringify({
+			title,
+			body,
+			author,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+		credentials: 'same-origin',
+	})
+
+	const json: BlogInt[] | [] = await response.json()
+
+	return json
+}
+
+export default createBlog
---
+++ src/API/Blogs/fetchAllBlogs.ts
@@ -0,0 +1,14 @@
+import { BlogInt } from '../../types'
+
+const fetchAllBlogs = async (): Promise<BlogInt[]> => {
+	const url = 'https://whispering-tundra-62913.herokuapp.com'
+
+	const response: Response = await fetch('/api/blogs', {
+		credentials: 'same-origin',
+	})
+	const json: BlogInt[] = await response.json()
+
+	return json
+}
+
+export default fetchAllBlogs
