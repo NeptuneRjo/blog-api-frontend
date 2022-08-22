27016b1e61ef5d22b13f81b5c889c9d2ee7fe197
NeptuneRjo <rjo9000@outlook.com>
2022-08-22 19:05:55 -0400

    move blog deletion to seperate component accessed in router

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchBlog, updateBlog, deleteBlog } from '../API/api-exports'
 import { CommentInt, BlogInt, CleanUserInt } from '../types'
-import { Comment, CommentForm } from '../components/components-exports'
+import {
+	Comment,
+	CommentForm,
+	DeleteBlog,
+} from '../components/components-exports'
 import { Button } from 'react-bootstrap'
 
 import formatDistanceToNow from 'date-fns/esm/formatDistanceToNow/index.js'
@@ -18,7 +22,7 @@ const Blog: React.FC<Props> = ({ user, setBlogs, blogs }: Props) => {
 
 	const [blog, setBlog] = useState<BlogInt>()
 	const [comments, setComments] = useState<CommentInt[] | []>([])
-	const [error, setError] = useState<any>()
+	const [error, setError] = useState<string | null>(null)
 
 	const [deleting, setDeleting] = useState<boolean>(false)
 
@@ -31,6 +35,7 @@ const Blog: React.FC<Props> = ({ user, setBlogs, blogs }: Props) => {
 				setError(json?.error)
 			} else if (response.ok) {
 				setBlog(json?.data)
+				setComments(json?.data?.comments)
 			}
 		})()
 	}, [])
@@ -44,7 +49,7 @@ const Blog: React.FC<Props> = ({ user, setBlogs, blogs }: Props) => {
 			body,
 		}
 
-		// Blog *should not* be undefined if on this page, but because blog can be undefined,
+		// Blog *should not* be undefined if on this page, but because // /// blog can be undefined,
 		// logic must be wrapped in if statement
 		if (blog !== undefined) {
 			const response = await updateBlog(newComment, blog, id)
@@ -54,70 +59,57 @@ const Blog: React.FC<Props> = ({ user, setBlogs, blogs }: Props) => {
 				setError(json?.error)
 			} else if (response.ok) {
 				setBlog(json?.data)
+				setComments(json?.data?.comments)
 			}
 		}
 	}
 
-	const handleBlogDelete = async () => {
-		const response = await deleteBlog(id as string)
-		const json = await response.json()
+	if (blog === undefined && error === null) {
+		return <div className=''>Loading...</div>
+	}
 
-		if (!response.ok) {
-			setError(json?.error)
-		} else if (response.ok) {
-			const deletedBlogId = json?.data?._id
-			const cleanedBlogArray = blogs.filter((blog) => {
-				return blog?._id !== deletedBlogId
-			})
-
-			setBlogs(cleanedBlogArray)
-		}
+	if (error) {
+		return (
+			<div>
+				<h4>404</h4>
+				<p>Page not found...</p>
+			</div>
+		)
 	}
 
 	return (
 		<div className='blog main'>
-			{blog !== undefined && (
-				<>
-					<h3 className='title'>{blog?.title}</h3>
-					<div className='blog body'>
-						<h6 className='blog author'>
-							Writen by {blog?.author} ~{' '}
-							{formatDistanceToNow(new Date(blog.date), { addSuffix: true })}
-						</h6>
-						<p className='blog text'>{blog?.body}</p>
-					</div>
-					{user !== undefined && user.role === 'Admin' && (
-						<div className='blog delete-field'>
-							{deleting === false && (
-								<Button onClick={() => setDeleting(true)}>Delete Blog</Button>
-							)}
-							{deleting === true && (
-								<>
-									<Button onClick={() => handleBlogDelete()}>Delete</Button>
-									<Button onClick={() => setDeleting(false)}>Cancel</Button>
-								</>
-							)}
+			<h3 className='title'>{blog?.title}</h3>
+			<div className='blog body'>
+				<h6 className='blog author'>
+					Writen by {blog?.author} ~{' '}
+					{formatDistanceToNow(new Date(blog?.date as string), {
+						addSuffix: true,
+					})}
+				</h6>
+				<p className='blog text'>{blog?.body}</p>
+			</div>
+			<div className='blog comments'>
+				<h3 className='title'>Comments</h3>
+				<div className='blog comments-new'>
+					<CommentForm handleCommentSubmit={handleCommentSubmit} user={user} />
+				</div>
+				{comments.length > 0 &&
+					comments.map((comment) => (
+						<div className='blog comments-grid'>
+							<Comment comment={comment} />
 						</div>
+					))}
+				{comments.length === 0 && (
+					<div className='blog comments-none'>No comments yet...</div>
+				)}
+			</div>
+			{user !== undefined && user.role === 'Admin' && (
+				<div className='blog delete-field'>
+					{deleting === false && (
+						<Button href={`#/delete-blog/${id}`}>Delete Blog</Button>
 					)}
-					<div className='blog comments'>
-						<h3 className='title'>Comments</h3>
-						<div className='blog comments-new'>
-							<CommentForm
-								handleCommentSubmit={handleCommentSubmit}
-								user={user}
-							/>
-						</div>
-						{comments.length > 0 &&
-							comments.map((comment) => (
-								<div className='blog comments-grid'>
-									<Comment comment={comment} />
-								</div>
-							))}
-						{comments.length === 0 && (
-							<div className='blog comments-none'>No comments yet...</div>
-						)}
-					</div>
-				</>
+				</div>
 			)}
 		</div>
 	)
