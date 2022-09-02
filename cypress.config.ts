503ce3faac5a8e770c1f502484b29d6c261a9ae7
NeptuneRjo <rjo9000@outlook.com>
2022-09-01 23:45:16 -0400

    cypress env

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+	e2e: {
+		setupNodeEvents(on, config) {
+			// implement node event listeners here
+		},
+	},
+	env: {
+		server: 'https://blog-api-3tmv.onrender.com',
+	},
+})
